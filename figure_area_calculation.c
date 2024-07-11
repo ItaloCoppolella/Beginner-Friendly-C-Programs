3987282313783490a5e0985bc964df62866eec70
ItaloCoppolella <97603806+ItaloCoppolella@users.noreply.github.com>
2024-07-11 20:10:19 +0200

    Add files via upload

---
+++ figure_area_calculation.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+    puts("1 for the area of the circle");  // Area del cerchio
+    puts("2 for the area of quadrate");     // Area del quadrato
+    puts("3 for the area of ​​the sphere");   // Area della sfera
+    puts("Insert a number for the choose: ");
+
+    unsigned int i;
+
+    scanf("%u", &i);
+
+    float x, y;
+
+    switch (i)
+    {
+    case 1:
+        puts("Insert the radius:");
+        scanf("%f", &x);
+        y = 3.14 * x * x;
+        printf("Area circle: %f", y);
+        break;
+
+    case 2:
+        puts("Insert the side:");
+        scanf("%f", &x);
+        y = 3.14 * x * x;
+        printf("Area quadrate: %f", y);
+
+    case 3:
+        puts("Insert the radius:");
+        scanf("%f", &x);
+        y = 4 * 3.14 * x * x;
+        printf("Area sphere: %f", y);
+        break;
+
+    default:
+        puts("Operazione non valida");
+        break;
+    }
+    return 0;
+}
